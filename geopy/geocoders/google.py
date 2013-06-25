798745d5d1095349f05d01115469ce24875ff29d
Alessandro Pasotti <apasotti@gmail.com>
2013-06-25 16:47:04 +0200

    Updated geopy and fixed googlev3 reverse (pulled upstream too)

--- geopy/geocoders/google.py
+++ geopy/geocoders/google.py
@@ -8,17 +8,16 @@ except ImportError:
     except ImportError:
         from django.utils import simplejson as json
 
-import xml
-from xml.parsers.expat import ExpatError
-
 from geopy.geocoders.base import Geocoder,GeocoderError,GeocoderResultError
 from geopy import Point, Location, util
 
+from warnings import warn
+
 class Google(Geocoder):
     """Geocoder using the Google Maps API."""
-
-    def __init__(self, api_key=None, domain='maps.google.com',
-                 resource=None, format_string='%s', output_format=None):
+    
+    def __init__(self, api_key=None, domain='maps.googleapis.com',
+                 format_string='%s'):
         """Initialize a customized Google geocoder with location-specific
         address information and your Google Maps API key.
 
@@ -29,40 +28,39 @@ class Google(Geocoder):
         is 'maps.google.com', but if you're geocoding address in the UK (for
         example), you may want to set it to 'maps.google.co.uk' to properly bias results.
 
-        ``resource`` is DEPRECATED and ignored -- the parameter remains for compatibility
-        purposes.  The supported 'maps/geo' API is used regardless of this parameter.
-
         ``format_string`` is a string containing '%s' where the string to
         geocode should be interpolated before querying the geocoder.
         For example: '%s, Mountain View, CA'. The default is just '%s'.
-
-        ``output_format`` (DEPRECATED) can be 'json', 'xml', or 'kml' and will
-        control the output format of Google's response. The default is 'json'. 'kml' is
-        an alias for 'xml'.
         """
-        if resource != None:
-            from warnings import warn
-            warn('geopy.geocoders.google.GoogleGeocoder: The `resource` parameter is deprecated '+
-                 'and now ignored. The Google-supported "maps/geo" API will be used.', DeprecationWarning)
-
-        if output_format != None:
-            from warnings import warn
-            warn('geopy.geocoders.google.GoogleGeocoder: The `output_format` parameter is deprecated.', DeprecationWarning)
+        
+        warn('geopy.geocoders.google: The `geocoders.google.Google` geocoder uses the '+
+            'older "V2" API and is deprecated and may be broken at any time. A '+
+            'geocoder utilizing the "V3" API is available at '+
+            '`geocoders.googlev3.GoogleV3` and will become the default in a future '+
+            'version. See RELEASES file and http://goo.gl/somDT for usage information.',
+            DeprecationWarning
+        )
+        
+        if not api_key:
+            raise ValueError(
+                "The `geocoders.google.Google` (V2) API now requires the "+
+                "`api_key` argument. Please acquire and use an API key "+
+                "(http://goo.gl/EdoHX) or upgrade to "+
+                "the V3 API (`geocoders.googlev3.GoogleV3`), which does "+
+                "not require a key. ---- Please note that the V2 API is " +
+                "deprecated and may not work after March 2013 or September 2013."
+            )
+        if domain == "maps.google.com":
+            raise ValueError(
+                "The `geocoders.google.Google` (V2) API now requires the "+
+                "`domain` argument to be set to 'maps.googleapis.com'. Please "+
+                "change or remove your `domain` kwarg."
+            )
 
         self.api_key = api_key
         self.domain = domain
         self.format_string = format_string
-
-        if output_format:
-            if output_format not in ('json','xml','kml'):
-                raise ValueError('if defined, `output_format` must be one of: "json","xml","kml"')
-            else:
-                if output_format == "kml":
-                    self.output_format = "xml"
-                else:
-                    self.output_format = output_format
-        else:
-            self.output_format = "xml"
+        self.output_format = "json"
 
     @property
     def url(self):
@@ -70,80 +68,25 @@ class Google(Geocoder):
         return "http://%s/maps/geo?%%s" % domain
 
     def geocode(self, string, exactly_one=True):
+        if isinstance(string, unicode):
+            string = string.encode('utf-8')
         params = {'q': self.format_string % string,
                   'output': self.output_format.lower(),
+                  'key': self.api_key,
+                  'sensor': False
                   }
-
-        if self.api_key:
-            params['key'] = self.api_key
-
+        
         url = self.url % urlencode(params)
         return self.geocode_url(url, exactly_one)
 
-    def reverse(self, coord, exactly_one=True):
-        if isinstance(coord, Point):
-            (lat, lng, _) = coord
-        else:
-            (lat, lng) = coord
-            params = {'q': self.format_string % lat+','+self.format_string % lng,
-                'output': self.output_format.lower()
-            }
-
-        if self.api_key:
-            # An API key is only required for the HTTP geocoder.
-            params['key'] = self.api_key
-
-        url = self.url % urlencode(params)
-        return self.geocode_url(url, exactly_one, reverse=True)
-
-    def geocode_url(self, url, exactly_one=True, reverse=False):
+    def geocode_url(self, url, exactly_one=True):
         util.logger.debug("Fetching %s..." % url)
         page = urlopen(url)
-
+        
         dispatch = getattr(self, 'parse_' + self.output_format)
-        return dispatch(page, exactly_one, reverse)
+        return dispatch(page, exactly_one)
 
-    def parse_xml(self, page, exactly_one=True, reverse=False):
-        """Parse a location name, latitude, and longitude from an XML response.
-        """
-        if not isinstance(page, basestring):
-            page = util.decode_page(page)
-        try:
-            doc = xml.dom.minidom.parseString(page)
-        except ExpatError:
-            places = []
-            doc = None
-        else:
-            places = doc.getElementsByTagName('Placemark')
-
-        if len(places) == 0 and doc is not None:
-            # Got empty result. Parse out the status code and raise an error if necessary.
-            status = doc.getElementsByTagName("Status")
-            status_code = int(util.get_first_text(status[0], 'code'))
-            self.check_status_code(status_code)
-
-        if (exactly_one and len(places) != 1) and (not reverse):
-	    raise ValueError("Didn't find exactly one placemark! " \
-			     "(Found %d.)" % len(places))
-
-        def parse_place(place):
-            location = util.get_first_text(place, ['address', 'name']) or None
-            points = place.getElementsByTagName('Point')
-            point = points and points[0] or None
-            coords = util.get_first_text(point, 'coordinates') or None
-            if coords:
-                longitude, latitude = [float(f) for f in coords.split(',')[:2]]
-            else:
-                latitude = longitude = None
-                _, (latitude, longitude) = self.geocode(location)
-            return (location, (latitude, longitude))
-
-        if exactly_one:
-            return parse_place(places[0])
-        else:
-            return [parse_place(place) for place in places]
-
-    def parse_json(self, page, exactly_one=True, reverse=True):
+    def parse_json(self, page, exactly_one=True):
         if not isinstance(page, basestring):
             page = util.decode_page(page)
         doc = json.loads(page)
@@ -155,7 +98,7 @@ class Google(Geocoder):
             status_code = status["code"]
             self.check_status_code(status_code)
             return None
-        elif exactly_one and len(places) != 1 and not reverse:
+        elif exactly_one and len(places) != 1:
             raise ValueError("Didn't find exactly one placemark! " \
                              "(Found %d.)" % len(places))
 
@@ -163,7 +106,7 @@ class Google(Geocoder):
             location = place.get('address')
             longitude, latitude = place['Point']['coordinates'][:2]
             return (location, (latitude, longitude))
-
+        
         if exactly_one:
             return parse_place(places[0])
         else:
--- geopy/tests/test_backends.py
+++ geopy/tests/test_backends.py
@@ -11,7 +11,7 @@ def _basic_address_test(self):
     
     try:
         clean_address, latlon = self.geocoder.geocode(address)
-    except URLError, e:
+    except URLError as e:
         if "timed out" in str(e).lower():
             raise unittest.SkipTest('geocoder service timed out')
         else:
@@ -25,7 +25,7 @@ def _partial_address_test(self):
     
     try:
         clean_address, latlon = self.geocoder.geocode(address)
-    except URLError, e:
+    except URLError as e:
         if "timed out" in str(e).lower():
             raise unittest.SkipTest('geocoder service timed out')
         else:
@@ -39,7 +39,7 @@ def _intersection_test(self):
     
     try:
         clean_address, latlon = self.geocoder.geocode(address)
-    except URLError, e:
+    except URLError as e:
         if "timed out" in str(e).lower():
             raise unittest.SkipTest('geocoder service timed out')
         else:
@@ -55,7 +55,7 @@ def _placename_test(self):
         # Since a place name search is significantly less accurate,
         # allow multiple results to come in. We'll check the top one.
         places = self.geocoder.geocode(address, exactly_one=False)
-    except URLError, e:
+    except URLError as e:
         if "timed out" in str(e).lower():
             raise unittest.SkipTest('geocoder service timed out')
         else:
@@ -64,9 +64,9 @@ def _placename_test(self):
     place = places[0]
     clean_address, latlon = place
     
-    # And since this is a pretty fuzzy search, we'll only test to .01
-    self.assertAlmostEqual(latlon[0], 46.1912, delta=.01)
-    self.assertAlmostEqual(latlon[1], -122.1944, delta=.01)
+    # And since this is a pretty fuzzy search, we'll only test to .02
+    self.assertAlmostEqual(latlon[0], 46.1912, delta=.02)
+    self.assertAlmostEqual(latlon[1], -122.1944, delta=.02)
     
 # ==========
 # Define the test cases that actually perform the import and instantiation step
@@ -75,6 +75,11 @@ class GoogleTestCase(unittest.TestCase):
     def setUp(self):
         from geopy.geocoders.google import Google
         self.geocoder = Google()
+
+class GoogleV3TestCase(unittest.TestCase):
+    def setUp(self):
+        from geopy.geocoders.googlev3 import GoogleV3
+        self.geocoder = GoogleV3()
 
 class BingTestCase(unittest.TestCase):
     def setUp(self):
@@ -100,6 +105,15 @@ class OpenMapQuestTestCase(unittest.TestCase):
     test_basic_address = _basic_address_test
     test_placename = _placename_test
 
+class MapQuestTestCase(unittest.TestCase):
+    def setUp(self):
+        from geopy.geocoders.mapquest import MapQuest
+        self.geocoder = MapQuest('Dmjtd%7Clu612007nq%2C20%3Do5-50zah')
+    
+    # Does not do fuzzy address search.
+    test_basic_address = _basic_address_test
+    test_placename = _placename_test
+
 class GeoNamesTestCase(unittest.TestCase):
     def setUp(self):
         from geopy.geocoders.geonames import GeoNames
@@ -108,7 +122,7 @@ class GeoNamesTestCase(unittest.TestCase):
     # Does not do any address searching.
     test_placename = _placename_test
 
-BASIC_TESTCASES = [GoogleTestCase, BingTestCase, DotUSTestCase, YahooTestCase]
+BASIC_TESTCASES = [GoogleTestCase, GoogleV3TestCase, BingTestCase, DotUSTestCase, YahooTestCase]
 
 # geonames does not actually test against addresses (just place names)
 #TESTCASES = [GoogleTestCase, BingTestCase, YahooTestCase, DotUSTestCase, GeoNamesTestCase]
@@ -138,6 +152,8 @@ def get_suite():
     
     tests.append(OpenMapQuestTestCase('test_basic_address'))
     tests.append(OpenMapQuestTestCase('test_placename'))
+    tests.append(MapQuestTestCase('test_basic_address'))
+    tests.append(MapQuestTestCase('test_placename'))
     tests.append(GeoNamesTestCase('test_placename'))
     
     return unittest.TestSuite(tests)
